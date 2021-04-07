7a91cb202476f2b1fb8567a22c4551a3cfba4517
Manoj Fernando <manojf@99x.lk>
2021-04-07 16:07:31 +0530

    Updated the card component and exported

--- my-scope/ui/card/card.jsx
+++ my-scope/ui/card/card.jsx
@@ -5,6 +5,7 @@ import { Button } from '@bitwave/demo-scope.ui.button';
 export const Card = ( {children, ...rest} ) => {
   return (
     <div {...rest}>
+      <h2>This is a label</h2>
       <Button>Click Here!</Button>
     </div>
   )
